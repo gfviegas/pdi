cde5cf30ea4798b0ec7f6119ddea3b30c7842f24
Bruno Marra de Melo <brunomarram@hotmail.com>
2019-04-04 09:00:22 -0300

    Remove código comentado

--- relatorio2/media_mediana.py
+++ relatorio2/media_mediana.py
@@ -20,10 +20,6 @@ median = cv2.medianBlur(image, 5)
 kernel = np.ones((5, 5), np.float32) / 25
 average = cv2.filter2D(image, -1, kernel)
 
-# Escreve o nome de cada filtro na imagem correspondente
-#font = cv2.FONT_HERSHEY_SIMPLEX
-#cv2.putText(median, 'Median', (60, 320), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
-
 # Salva a imagem
 cv2.imwrite("mediana.png", median)
 cv2.imwrite("media.png", average)
